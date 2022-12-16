a29b8a809e40ba406f52514866c6ae03c898ad70
boangus <weiboanan@163.com>
2022-12-16 21:29:50 +0800

    Initial commit

---
+++ R/Fvalue.R
@@ -0,0 +1,36 @@
+#' Calculate F value
+#'
+#' @param data expose data,it's better to use TwosampleMR read_exposed_data's data
+#' @param eaf frequency col
+#' @param beta effective col
+#' @param se se col
+#' @param N samplesize col
+#' @param path the way you save the expose data
+#'
+#' @return the total F value, and a file of the expose data which includ the F value respectively
+#' and a select column which based on the F value more than 10.
+#' @importFrom utils write.csv
+#' @export Fval
+
+
+Fval <- function(data,eaf="eaf.exposure",beta="beta.exposure",se="se.exposure",N="samplesize.exposure",path="Fval.csv"){
+    eaf <- data[,eaf]
+    beta <- data[,beta]
+    se <- data[,se]
+    N <- data[,N]
+    beta_sd <- beta/(se*sqrt(N))
+    R2 <- 2*(1-eaf)*eaf*beta_sd*beta_sd
+    F_val <- (N-2)*R2/(1-R2)
+
+    data$R2 <- R2
+    data$Fval <- F_val
+
+    r2 <- sum(R2)
+    Ftotal <- (r2/(1-r2))*(N-nrow(data)-1)/nrow(data)
+    print(paste("Ftotal",Ftotal[nrow(data)]))
+    data$select <- ifelse(data$Fval>10,"T","F")
+    write.csv(data,path,row.names = F)
+    data <- data[data$Fval>10,]
+    return(data)
+}
+
---
+++ R/perform_MR.R
@@ -0,0 +1,51 @@
+#' perform MR for one code
+#'
+#' @param data After using TwosampleMR harmorize's data
+#' @param path which folder you want to save
+#' @param save_name the name you want to save
+#' @param phenoscanner to check the snp in phenoscanner
+#' @param MR_CSV to perform mr and write it to csv
+#' @param MR_PIC to get the picture
+#'
+#' @return three csv file and a PDF which includ three picture
+#' @importFrom grDevices dev.off pdf
+#' @importFrom utils write.csv
+#' @export perform_MR
+
+perform_MR <- function(data,path="",save_name="exposure_outcome",
+                       phenoscanner=F,MR_CSV=T,MR_PIC=T){
+    if(phenoscanner){
+        res <- phenoscanner::phenoscanner(snpquery=data$SNP)
+        write.csv(res$results,paste(path,"PS_",save_name,".csv",sep = ""))
+    }
+
+
+    if(MR_CSV){
+        TwoSampleMR::generate_odds_ratios(mr_res = TwoSampleMR::mr(data)) |>
+            write.csv(paste(path,"MR_",save_name,".csv",sep = ""))
+
+        TwoSampleMR::mr_pleiotropy_test(data) |>
+            write.csv(paste(path,"PLE_",save_name,".csv",sep = ""))
+
+        TwoSampleMR::mr_heterogeneity(data) |>
+            write.csv(paste(path,"HET_",save_name,".csv",sep = ""))
+    }
+
+    if(MR_PIC){
+        pdf(paste(path,"PIC_",save_name,".pdf",sep = ""))
+
+        p1<- TwoSampleMR::mr_scatter_plot(mr_results = TwoSampleMR::mr(data,method_list =  c("mr_ivw","mr_egger_regression","mr_weighted_median")),data)
+        print(p1)
+
+        p2 <- TwoSampleMR::mr_funnel_plot(singlesnp_results = TwoSampleMR::mr_singlesnp(data))
+        print(p2)
+
+        p3 <- TwoSampleMR::mr_leaveoneout_plot(leaveoneout_results = TwoSampleMR::mr_leaveoneout(data))
+        print(p3)
+
+        dev.off()
+
+    }
+
+}
+
